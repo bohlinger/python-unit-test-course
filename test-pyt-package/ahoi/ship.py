d07f4d6531ebb0fb97c6ea68758c1b2a6282e890
Patrik Bohlinger <patrikb@pc5591.pc.met.no>
2022-10-04 12:36:25 +0200

    added .gitignore, added tests

--- test-pyt-package/ahoi/ship.py
+++ test-pyt-package/ahoi/ship.py
@@ -16,6 +16,20 @@ class Field:
         return self.xx, self.yy
 
 
+def test_docs_Field():
+    """
+    Plots field for testing doctest-module
+
+    >>> f = Field(np.linspace(0, 100), np.linspace(0, 100))
+    >>> assert f.xx.shape == (50, 50)
+    """
+    return
+
+def test_np_assert():
+    A = np.ones([10,10])
+    B = A * 0.999
+    np.testing.assert_allclose(A,B,rtol=1e-2)
+
 def test_field():
     f = Field(np.linspace(0, 100), np.linspace(0, 100))
     assert f.xx.shape == (50, 50)
--- test-pyt-package/tests/test_load.py
+++ test-pyt-package/tests/test_load.py
@@ -1,2 +1,17 @@
+from ahoi.ship import *
+import numpy
+
 def test_load_data(norkyst):
     print(norkyst)
+
+def test_field():
+    f = Field(np.linspace(0, 100), np.linspace(0, 100))
+    assert f.xx.shape == (50, 50)
+
+def test_field_plot(plot):
+    f = Field(np.linspace(0, 100), np.linspace(0, 100))
+    A = np.ones([100,100])
+    if plot:
+        import matplotlib.pyplot as plt
+        plt.imshow(A)
+        plt.show()
